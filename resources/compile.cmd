76e97bcea8c3fe2dcdf7e3b19a9f5e8a3e85cf1a
nicarley <nicarley@gmail.com>
2025-06-11 16:35:25 -0500

    Version 2025.06.11

--- resources/compile.cmd
+++ resources/compile.cmd
@@ -1,2 +1,2 @@
-pyinstaller --noconfirm --onedir --windowed --icon "C:\Users\admin7\OneDrive - Americana Building Products\Projects\PythonWeather\resources\icon.ico" --name "Weather Alert Monitor" --add-data "C:\Users\admin7\OneDrive - Americana Building Products\Projects\PythonWeather\resources;resources/"  "C:\Users\admin7\OneDrive - Americana Building Products\Projects\PythonWeather\PyWeatherAlertGUI.py"
+pyinstaller --noconfirm --onedir --windowed --icon "..\resources\icon.ico" --name "Weather Alert Monitor" --add-data "../resources;resources/"  "..\PyWeatherAlertGUI.py"
 
